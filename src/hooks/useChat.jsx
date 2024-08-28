2f6af1b5f821d0544dbc84a1a29bbbddc9c5de2c
赵晨炀 <904852749@qq.com>
2024-08-28 19:59:20 +0800

    feat: env

--- src/hooks/useChat.jsx
+++ src/hooks/useChat.jsx
@@ -1,59 +1,60 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-const backendUrl = "http://localhost:3000/api";
+const backendUrl = process.env.NEXT_PUBLIC_APP_API;
 
 const ChatContext = createContext();
 
 export const ChatProvider = ({ children }) => {
-	const chat = async (message) => {
-		setLoading(true);
-		const data = await fetch(`${backendUrl}/chat`, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ message }),
-		});
-		const resp = (await data.json()).messages;
-		setMessages((messages) => [...messages, ...resp]);
-		setLoading(false);
-	};
-	const [messages, setMessages] = useState([]);
-	const [message, setMessage] = useState();
-	const [loading, setLoading] = useState(false);
-	const [cameraZoomed, setCameraZoomed] = useState(true);
-	const onMessagePlayed = () => {
-		setMessages((messages) => messages.slice(1));
-	};
+    const chat = async (message) => {
+        setLoading(true);
 
-	useEffect(() => {
-		if (messages.length > 0) {
-			setMessage(messages[0]);
-		} else {
-			setMessage(null);
-		}
-	}, [messages]);
+        const data = await fetch(`${backendUrl}/chat`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ message }),
+        });
+        const resp = (await data.json()).messages;
+        setMessages((messages) => [...messages, ...resp]);
+        setLoading(false);
+    };
+    const [messages, setMessages] = useState([]);
+    const [message, setMessage] = useState();
+    const [loading, setLoading] = useState(false);
+    const [cameraZoomed, setCameraZoomed] = useState(true);
+    const onMessagePlayed = () => {
+        setMessages((messages) => messages.slice(1));
+    };
 
-	return (
-		<ChatContext.Provider
-			value={{
-				chat,
-				message,
-				onMessagePlayed,
-				loading,
-				cameraZoomed,
-				setCameraZoomed,
-			}}
-		>
-			{children}
-		</ChatContext.Provider>
-	);
+    useEffect(() => {
+        if (messages.length > 0) {
+            setMessage(messages[0]);
+        } else {
+            setMessage(null);
+        }
+    }, [messages]);
+
+    return (
+        <ChatContext.Provider
+            value={{
+                chat,
+                message,
+                onMessagePlayed,
+                loading,
+                cameraZoomed,
+                setCameraZoomed,
+            }}
+        >
+            {children}
+        </ChatContext.Provider>
+    );
 };
 
 export const useChat = () => {
-	const context = useContext(ChatContext);
-	if (!context) {
-		throw new Error("useChat must be used within a ChatProvider");
-	}
-	return context;
+    const context = useContext(ChatContext);
+    if (!context) {
+        throw new Error("useChat must be used within a ChatProvider");
+    }
+    return context;
 };
